50f249bfa09e83498925f8b9cbb8a45732eca0cd
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2021-11-18 09:17:16 -0400

    adding functions to play, pause, next

--- js/script.js
+++ js/script.js
@@ -11,3 +11,49 @@ const progressBar = wrapper.querySelector('.progress-bar');
 const musicList = wrapper.querySelector('.music-list');
 const moreMusicBtn = wrapper.querySelector('#more-music');
 const closeMoreMusic = wrapper.querySelector('#close');
+
+let musicIndex = Math.floor(Math.random() * allMusic.length + 1);
+let isMusicPaused = true;
+
+window.addEventListener('load', () => {
+  loadMusic(musicIndex);
+});
+
+function loadMusic(indexNumb) {
+  musicName.innerText = allMusic[indexNumb - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
+  mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+/* play music function */
+function playMusic() {
+  wrapper.classList.add('paused');
+  playPauseBtn.querySelector('i').innerText = 'pause';
+  mainAudio.play();
+}
+
+/* pause music function */
+function pauseMusic() {
+  wrapper.classList.remove('paused');
+  playPauseBtn.querySelector('i').innerText = 'play_arrow';
+  mainAudio.pause();
+}
+
+/* prev music function */
+function prevMusic() {
+  musicIndex--;
+  musicIndex < 1 ? (musicIndex = allMusic.length) : (musicIndex = musicIndex);
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
+
+/* next music function */
+function nextMusic() {
+  musicIndex++;
+  musicIndex > allMusic.length ? (musicIndex = 1) : (musicIndex = musicIndex);
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
